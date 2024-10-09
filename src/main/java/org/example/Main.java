dfbcdc4654a55d8a265361992f0ff2015a97e446
Vladimir902 <vladimir.malakovski@kinandcarta.com>
2024-10-09 13:15:42 +0200

    reverted the commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! i si rabotime ovdde da");
+        System.out.println("Hello world! i si rabotime ovdde ???");
+        System.out.println("uste nesto ovde");
+        System.out.println("abe ajde");
+        System.out.println("nekoja izmena, lets see lets goo");
+        int x = 0;
+
+        System.out.println("Agabaa");
+        String changes = "I want to do some changes";
+        System.out.println("I wan to do changee");
     }
 }
